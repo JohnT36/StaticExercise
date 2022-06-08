ec9f9673d57a51b98b99de11012c6bdbae2da599
John Tuck <j.tuck1336@gmail.com>
2022-06-08 08:34:48 -0400

    Static Exercise take 1

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelcius(68);
+                Console.WriteLine($"It is {celcius} degrees in celcius!");
+
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20);
+            Console.WriteLine($"It is {fahrenheit} degrees in fahrenhiet!");
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double CelciusToFahrenheit(double celcius)
+        {
+            var result = (celcius * 9) / 5 + 32;
+            return result;
+
+        }
+
+
+
+        public static double FahrenheitToCelcius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32) / 1.8;
+            return result;
+
+        }
+
+
+
+
+
+    }
+    
+
+
+
+}
